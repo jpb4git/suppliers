4cfe18c35e130b6ae11502291104a3c617a77b87
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-05-06 16:00:17 +0200

    exo 7.3 add markers done

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
   <h1>Suppliers Map</h1>
-  <gmap-map
-      :center="{lat:10, lng:10}"
-      :zoom="7"
-      style="width: 100%; height: 800px"
-    >
-  </gmap-map>
+  <b-container>
+    <b-row>
+      <b-col>
+          <gmap-map :center="{lat:latitude, lng:longitude}" :zoom="7" style="width: 100%; height: 400px">
+          <GmapMarker 
+              v-for="supplier in suppliers" 
+              :position="{lat:supplier.latitude,lng:supplier.longitude}" :key="supplier.id">
+            </GmapMarker>
+          </gmap-map>
+      </b-col>
+    </b-row>
+  </b-container>
   </div>
 </template>
 
@@ -15,6 +21,39 @@
 
 export default {
   name: 'Map',
+  data : function (){
+    return {
+              latitude:48.55,
+              longitude: 3.01, 
+            suppliers: 
+            [
+              {
+                id: 1,
+                latitude:48.55,
+                longitude: 3.01   
+              },
+              {
+                id: 2,
+                latitude: 48.55,
+                longitude: 3.9  
+              }
+            ]
+      }
+
+  },
   
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.b-container{
+  .b-row{
+    .b-col{
+      background-color: aquamarine!important;
+    }  
+    
+  }
+  
+}
+</style>
