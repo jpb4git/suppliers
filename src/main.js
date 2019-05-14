8c29ffad73c2001608cce0a4810f89278f8498f5
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-05-14 10:01:20 +0200

    install @vue/pwa

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 //Google librairy 
 import * as VueGoogleMaps from 'vue2-google-maps'
+
+import './registerServiceWorker'
 Vue.use(VueGoogleMaps, {
   load: {
     key:  process.env.VUE_APP_API_TOKEN,
