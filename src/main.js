795de59644cf8f9fdf733c9393451edc5743532a
Jean-Pascal Boudet <dev@guitaresphere.com>
2019-05-15 11:55:17 +0200

    config pwa and workbox

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ import store from './store'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 //Google librairy 
 import * as VueGoogleMaps from 'vue2-google-maps'
+
+//  vue add @vue/pwa
+import './registerServiceWorker'
+
 Vue.use(VueGoogleMaps, {
   load: {
     key:  process.env.VUE_APP_API_TOKEN,
